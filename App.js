f368c958294665fece25ac5842ed0d538df3eca4
june <june.pangg@gmail.com>
2023-07-28 01:21:43 +0800

    optimize setting of tabBarIcon

--- App.js
+++ App.js
@@ -15,17 +15,42 @@ const BottomTab = createBottomTabNavigator();
 function App() {
     return (
         <NavigationContainer>
-            <BottomTab.Navigator screenOptions={{ headerShown: false }}>
+            <BottomTab.Navigator
+                screenOptions={({ route }) => ({
+                    headerShown: true,
+                    tabBarIcon: ({ color, size }) => {
+                        let ioniconName;
+                        switch (route.name) {
+                            case 'Home':
+                                ioniconName = 'home';
+                                break;
+                            case 'Discover':
+                                ioniconName = 'compass';
+                                break;
+                            case 'My Watchlists':
+                                ioniconName = 'tv';
+                                break;
+                            case 'Settings':
+                                ioniconName = 'settings';
+                                break;
+                            default:
+                                break;
+                        }
+                        return (
+                            <Ionicons
+                                name={ioniconName}
+                                color={color}
+                                size={size}
+                            />
+                        );
+                    },
+                })}>
                 {/* Home Screen Tab */}
                 <BottomTab.Screen
                     name="Home"
                     component={HomeScreen}
                     options={{
                         tabBarLabel: 'Home',
-                        // use Ionicon's home icon
-                        tabBarIcon: ({ color, size }) => (
-                            <Ionicons name="home" color={color} size={size} />
-                        ),
                     }}
                 />
                 {/* Discover Screen Tab */}
@@ -34,26 +59,14 @@ function App() {
                     component={DiscoverScreen}
                     options={{
                         tabBarLabel: 'Discover',
-                        // use Ionicon's settings icon
-                        tabBarIcon: ({ color, size }) => (
-                            <Ionicons
-                                name="compass"
-                                color={color}
-                                size={size}
-                            />
-                        ),
                     }}
                 />
                 {/* Watchlist Screen Tab */}
                 <BottomTab.Screen
-                    name="MyLists"
+                    name="My Watchlists"
                     component={WatchlistScreen}
                     options={{
                         tabBarLabel: 'Watchlists',
-                        // use Ionicon's settings icon
-                        tabBarIcon: ({ color, size }) => (
-                            <Ionicons name="tv" color={color} size={size} />
-                        ),
                     }}
                 />
                 {/* Settings Screen Tab */}
@@ -62,14 +75,6 @@ function App() {
                     component={SettingsScreen}
                     options={{
                         tabBarLabel: 'Settings',
-                        // use Ionicon's settings icon
-                        tabBarIcon: ({ color, size }) => (
-                            <Ionicons
-                                name="settings"
-                                color={color}
-                                size={size}
-                            />
-                        ),
                     }}
                 />
             </BottomTab.Navigator>
