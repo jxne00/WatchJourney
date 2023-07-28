82f31b3d51bb74c3997c5cb99cc1c1c3578a7fe7
june <june.pangg@gmail.com>
2023-07-28 21:38:37 +0800

    custom button and text components

--- components/CustomBtn.js
+++ components/CustomBtn.js
@@ -1,38 +1,51 @@
 import React from 'react';
-import { Text, StyleSheet, Pressable } from 'react-native';
+import { View, Text, StyleSheet, TouchableOpacity } from 'react-native';
+import { LinearGradient } from 'expo-linear-gradient';
 
 import Constants from '../constants/constants';
 
 // renders a custom button
-const CustomBtn = ({ text, onPress, align }) => {
-    // align center by default
-    alignButton = align ? align : 'center';
+const CustomBtn = (props) => {
+    // set default values to use if none passed in
+    alignButton = props.align || 'center';
+    colors = props.colors || ['#fff', '#868686', '#000'];
+    start = props.start || { x: 0, y: 0 };
+    end = props.end || { x: 1, y: 0 };
+    borderWidth = props.borderCol ? 1 : 0;
+
     return (
-        <Pressable
-            style={[styles.button, { alignSelf: alignButton }]}
-            onPress={onPress}>
-            <Text style={styles.text}>{text}</Text>
-        </Pressable>
+        <TouchableOpacity
+            style={[
+                styles.container,
+                {
+                    alignSelf: alignButton,
+                    borderColor: props.borderCol,
+                    borderWidth: borderWidth,
+                },
+            ]}
+            onPress={props.onPress}>
+            <LinearGradient
+                colors={colors}
+                start={start}
+                end={end}
+                style={styles.LnrGradient}>
+                <Text style={styles.text}>{props.text}</Text>
+            </LinearGradient>
+        </TouchableOpacity>
     );
 };
 
 const styles = StyleSheet.create({
-    button: {
-        alignItems: 'center',
-        justifyContent: 'center',
-        borderRadius: 12,
-        paddingHorizontal: 20,
-        paddingVertical: 10,
+    container: {
         width: '30%',
-        backgroundColor: Constants.PRIMARY_COL,
-        shadowColor: '#000',
-        shadowOffset: {
-            width: -3,
-            height: 2,
-        },
-        shadowOpacity: 0.5,
-        shadowRadius: 2.5,
-        elevation: 5,
+        height: 40,
+        borderRadius: 15,
+    },
+    LnrGradient: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        borderRadius: 15,
     },
     text: {
         fontFamily: Constants.POPPINS_SEMIBOLD_FONT,
---
+++ components/GradientText.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import { Text, StyleSheet } from 'react-native';
+import { LinearGradient } from 'expo-linear-gradient';
+import MaskedView from '@react-native-masked-view/masked-view';
+
+const GradientText = (props) => {
+    // use default colors if none passed in
+    const defaultColors = ['#ffffff', '#868686', '#000000'];
+    const colors = props.colors || defaultColors;
+    // use default start and end points if none passed in
+    const start = props.start || { x: 0, y: 0 };
+    const end = props.end || { x: 1, y: 0 };
+
+    return (
+        <MaskedView maskElement={<Text {...props} />}>
+            <LinearGradient colors={colors} start={start} end={end}>
+                <Text {...props} style={[props.style, styles.text]} />
+            </LinearGradient>
+        </MaskedView>
+    );
+};
+
+const styles = StyleSheet.create({
+    text: {
+        opacity: 0,
+    },
+});
+
+export default GradientText;
