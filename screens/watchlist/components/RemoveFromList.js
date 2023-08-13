c580e7370d97c25ab1a0be85adf82172e6c2f673
june <86581908+jxne00@users.noreply.github.com>
2023-08-13 23:33:02 +0800

    add unique user id to async key

--- screens/watchlist/components/RemoveFromList.js
+++ screens/watchlist/components/RemoveFromList.js
@@ -1,5 +1,18 @@
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import { fetchFromAsyncStorage } from '../../../components/AsyncActions';
+import { auth } from '../../../data/Firebase';
+
+/**
+ * @description Get the firebase userID of current loggedin user
+ */
+const getUserId = async () => {
+  try {
+    const user = await auth.currentUser;
+    return user.uid;
+  } catch (err) {
+    console.error('getUserId(): ', err);
+  }
+};
 
 /**
  * @description Remove a movie or TV show ID from a watchlist in AsyncStorage
@@ -10,30 +23,40 @@ import { fetchFromAsyncStorage } from '../../../components/AsyncActions';
  */
 const removeIDfromList = async (watchlist, id, setStateItem, type) => {
   try {
+    userID = await getUserId();
+
     // Retrieve the current list of IDs
     const storedIDs =
-      JSON.parse(await AsyncStorage.getItem(`@${watchlist}_${type}list`)) || [];
+      JSON.parse(
+        await AsyncStorage.getItem(`@${userID}_${watchlist}_${type}list`),
+      ) || [];
 
     // Filter out the ID to remove
     const updatedIDs = storedIDs.filter((storedID) => storedID !== id);
 
     // Store the updated list back in AsyncStorage
     await AsyncStorage.setItem(
-      `@${watchlist}_${type}list`,
+      `@${userID}_${watchlist}_${type}list`,
       JSON.stringify(updatedIDs),
     );
 
     // Fetch the updated list of IDs from AsyncStorage
     const updatedList = await fetchFromAsyncStorage(
-      `@${watchlist}_${type}list`,
+      `@${userID}_${watchlist}_${type}list`,
     );
     setStateItem(updatedList);
 
-    console.log(id, 'removed from', `@${watchlist}_${type}list`);
+    console.log(id, 'removed from', `@${userID}_${watchlist}_${type}list`);
   } catch (err) {
     type === 'movie'
-      ? console.log(`Error removing movie from ${watchlist} list: `, err)
-      : console.log(`Error removing TV show from ${watchlist} list: `, err);
+      ? console.log(
+          `Error removing movie from "${userID}_${watchlist}" list: `,
+          err,
+        )
+      : console.log(
+          `Error removing TV show from "${userID}_${watchlist}" list: `,
+          err,
+        );
   }
 };
 
